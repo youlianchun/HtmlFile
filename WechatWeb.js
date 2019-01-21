a6dd3e513b121bdf53a2e49a59ba439306c71c96
youlianchun <youlianchun@icloud.com>
2019-01-21 10:39:54 +0800

    Update WechatWeb.js

--- WechatWeb.js
+++ WechatWeb.js
@@ -1,5 +1,5 @@
 //jinyou.qqq.ybkc365.com(传影联盟) AD remove
-if (window.location.host == 'jinyou.qqq.ybkc365.com') {
+if (window.location.host.match(new RegExp("^jinyou.qqq.*$"))) {
     $.map($('body *'), function(e,n) {
           　　if ($(e).css('position') != 'static' && $(e).css('z-index') > 900)
           　　 $(e).remove();
